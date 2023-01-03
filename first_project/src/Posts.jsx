1bd8abbd53241d13b7a55cd552daab98d3258135
jiye-7 <jy522hi@gmail.com>
2023-01-03 14:58:29 +0900

    feat: pagination
    - queryKey에 currentPage 정보 추가

--- first_project/src/Posts.jsx
+++ first_project/src/Posts.jsx
@@ -2,23 +2,27 @@ import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import PostDetail from './PostDetail';
 
-const fetchPosts = async () => {
+const maxPostPage = 10; // 100개의 data만 제공해주는데 limit을 10으로 요청하기 때문에 마지막 페이지를 10으로 설정
+
+const fetchPosts = async (pageNum) => {
   const response = await fetch(
-    'https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0'
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0${pageNum}`
   );
   return response.json();
 };
 
 const Posts = () => {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // 기본값으로 3번 시도(횟수는 변경 가능)
   const { isLoading, isError, error, data } = useQuery({
-    queryKey: ['posts'],
-    queryFn: fetchPosts,
+    queryKey: ['posts', currentPage],
+    queryFn: () => fetchPosts(currentPage),
     staleTime: 2000,
   });
+
+  console.log(data);
 
   return (
     <>
@@ -41,11 +45,17 @@ const Posts = () => {
         ))}
       </ul>
       <div className='pages'>
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => setCurrentPage((previousValue) => previousValue - 1)}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => setCurrentPage((previousValue) => previousValue + 1)}
+        >
           Next page
         </button>
       </div>

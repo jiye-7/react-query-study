78a4e26c4b4dbb9d9478cd6711a4def2e17895ec
jiye-7 <jy522hi@gmail.com>
2023-01-03 15:50:20 +0900

    feat: prefetchQuery
    - useQueryClient hook의 prefetchQuery를 사용하여 Cache에 미리 넣는 방식으로 활용

--- first_project/src/Posts.jsx
+++ first_project/src/Posts.jsx
@@ -1,6 +1,12 @@
-import { useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import PostDetail from './PostDetail';
+
+/**
+ * pagination을 구현 -> loading indicator로인해 사용자 경험이 좋지 않은 이슈 -> 페이지에 캐시가 없기 때문에 next page를 누를 때 마다 페이지가 로딩되길 기다려야 했다.
+ * 해결: 데이터를 미리 가져와 캐시에 넣어서 사용자가 기다릴 필요 없도록 해결
+ * Prefetching: data를 cache에 추가하며 구성할 수 있지만 기본값으로 stale(만료) 상태, queryClient의 prefetchQuery 메소드를 사용
+ */
 
 const maxPostPage = 10; // 100개의 data만 제공해주는데 limit을 10으로 요청하기 때문에 마지막 페이지를 10으로 설정
 
@@ -15,11 +21,27 @@ const Posts = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    // 9페이지 이전이라면 prefetching이 이루어지지만 10페이지라면 미리 가져올 데이터가 없다.
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+
+      queryClient.prefetchQuery({
+        queryKey: ['posts', nextPage],
+        queryFn: () => fetchPosts(nextPage),
+      });
+    }
+  }, [currentPage, queryClient]);
+
   // 기본값으로 3번 시도(횟수는 변경 가능)
   const { isLoading, isError, error, data } = useQuery({
     queryKey: ['posts', currentPage],
     queryFn: () => fetchPosts(currentPage),
     staleTime: 2000,
+    // queryKey가 바뀔 때도 지난 데이터를 유지해서 이전 페이지로 돌아갔을 경우에 Cache에 해당 데이터가 있도록 만들고 싶다.
+    keepPreviousData: true,
   });
 
   console.log(data);

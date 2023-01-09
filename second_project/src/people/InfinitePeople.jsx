939bf606d1064aded97f532976af745230eea2c1
jiye-7 <jy522hi@gmail.com>
2023-01-09 19:20:45 +0900

    feat: data fetching, loading 일 때 사용자에게 보여주도록 처리

--- second_project/src/people/InfinitePeople.jsx
+++ second_project/src/people/InfinitePeople.jsx
@@ -11,36 +11,39 @@ const fetchUrl = async ({ pageParam = initialUrl }) => {
   return response;
 };
 
-/**
- * isLoading(불러오는 중)을 사용해서 cached된 데이터가 없을 때 데이터를 가져 온다.
- * isError: error가 있을 때
- */
 const InfinitePeople = () => {
-  const { data, fetchNextPage, hasNextPage, isLoading, isError, error } =
-    useInfiniteQuery({
-      queryKey: ['sw-people'],
-      queryFn: fetchUrl,
-      getNextPageParam: (lastPage) => lastPage.data.next || undefined,
-    });
-
-  if (isLoading) <div className='loading'>Loading...</div>;
-  if (isError) <div>Error! error Message: {error.toString()}</div>;
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isFetching,
+    isError,
+    error,
+  } = useInfiniteQuery({
+    queryKey: ['sw-people'],
+    queryFn: fetchUrl,
+    getNextPageParam: (lastPage) => lastPage.data.next || undefined,
+  });
 
   return (
-    <InfiniteScroll loadMore={() => fetchNextPage()} hasMore={hasNextPage}>
+    <>
+      {isFetching && <div className='fetching'>Loading....</div>}
       {isLoading && <div className='loading'>Loading...</div>}
       {isError && <div>Error! error Message: {error.toString()}</div>}
-      {data?.pages?.map((pageData) =>
-        pageData?.data?.results?.map((person) => (
-          <Person
-            key={person.name}
-            name={person.name}
-            hairColor={person.hair_color}
-            eyeColor={person.eye_color}
-          />
-        ))
-      )}
-    </InfiniteScroll>
+      <InfiniteScroll loadMore={() => fetchNextPage()} hasMore={hasNextPage}>
+        {data?.pages?.map((pageData) =>
+          pageData?.data?.results?.map((person) => (
+            <Person
+              key={person.name}
+              name={person.name}
+              hairColor={person.hair_color}
+              eyeColor={person.eye_color}
+            />
+          ))
+        )}
+      </InfiniteScroll>
+    </>
   );
 };
 

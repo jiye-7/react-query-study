a31cbdd94b1c832128a86fcf1494127c1ea188ae
jiye-7 <jy522hi@gmail.com>
2023-01-16 18:16:49 +0900

    fix: cache가 비어있는 경우 아무것도 표시하지 않도록 처리
    fallback data 값을 빈 배열로 지정하고, useQuery의 구조분해 된 data에 해당 fallback을 지정

--- third_project/client/src/components/treatments/hooks/useTreatments.ts
+++ third_project/client/src/components/treatments/hooks/useTreatments.ts
@@ -12,8 +12,10 @@ export const getTreatments = async (): Promise<Treatment[]> => {
 };
 
 export function useTreatments(): Treatment[] {
-  // TODO: get data from server via useQuery
-  const { data } = useQuery({
+  // /treatments 요청에 대한 응답값으로 cache가 비어있는 경우 아무것도 표시하지 않도록 처리할 것이다.
+  const fallback = [];
+  // 구조분해 된 data에 대해 기본값을 fallback으로 지정
+  const { data = fallback } = useQuery({
     queryKey: [queryKeys.treatments],
     queryFn: getTreatments,
   });

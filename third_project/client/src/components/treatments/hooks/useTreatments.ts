7bf6fddc44aee7bc1ef2b2f28f14b2df038e5798
jiye-7 <jy522hi@gmail.com>
2023-01-18 23:49:43 +0900

    feat: queryClient에 대한 onError: 집중식 오류 핸들링하기

--- third_project/client/src/components/treatments/hooks/useTreatments.ts
+++ third_project/client/src/components/treatments/hooks/useTreatments.ts
@@ -3,33 +3,19 @@ import { useQuery } from '@tanstack/react-query';
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
 
-// for when we need a query function for useQuery
 export const getTreatments = async (): Promise<Treatment[]> => {
   const { data } = await axiosInstance.get('/treatments');
   return data;
 };
 
 export function useTreatments(): Treatment[] {
-  // toast
-  const toast = useCustomToast();
-
   // /treatments 요청에 대한 응답값으로 cache가 비어있는 경우 아무것도 표시하지 않도록 처리할 것이다.
   const fallback = [];
   // 구조분해 된 data에 대해 기본값을 fallback으로 지정
   const { data = fallback } = useQuery({
     queryKey: [queryKeys.treatments],
     queryFn: getTreatments,
-    onError: (error) => {
-      // error가 JS Error class의 인스턴스라면
-      const title =
-        error instanceof Error
-          ? error.message
-          : 'error connecting to the server';
-
-      toast({ title, status: 'error' });
-    },
   });
 
   return data;
--- third_project/client/src/react-query/queryClient.ts
+++ third_project/client/src/react-query/queryClient.ts
@@ -1,22 +1,30 @@
+import { createStandaloneToast } from '@chakra-ui/react';
 import { QueryClient } from '@tanstack/react-query';
-// import { createStandaloneToast } from '@chakra-ui/react';
-// import { theme } from '../theme';
 
-// const toast = createStandaloneToast({ theme });
+import { theme } from '../theme';
+
+const toast = createStandaloneToast({ theme });
 
 /**
  * 에러 핸들러 및 기타 추가할 초기값들을 독립적인 파일로 분리
  */
 
-// function queryErrorHandler(error: unknown): void {
-//   // error is type unknown because in js, anything can be an error (e.g. throw(5))
-//   const title =
-//     error instanceof Error ? error.message : 'error connecting to server';
+function queryErrorHandler(error: unknown): void {
+  // error is type unknown because in js, anything can be an error (e.g. throw(5))
+  const title =
+    error instanceof Error ? error.message : 'error connecting to server';
 
-//   // prevent duplicate toasts
-//   toast.closeAll();
-//   toast({ title, status: 'error', variant: 'subtle', isClosable: true });
-// }
+  // prevent duplicate toasts
+  toast.closeAll();
+  // standalone toast에서는 custom toast를 사용할 수 없어서 custom hook과 다르게 몇 가지 옵션을 추가
+  toast({ title, status: 'error', variant: 'subtle', isClosable: true });
+}
 
 // to satisfy typescript until this file has uncommented contents
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      onError: queryErrorHandler,
+    },
+  },
+});
